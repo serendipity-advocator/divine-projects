b6186defbb99435201b0b6eea72a5782aed4ff77
Serendipity_advocator <jason24988114@gapp.nthu.edu.tw>
2019-10-03 23:16:32 +0800

    Escape

--- tetris.cpp
+++ tetris.cpp
@@ -39,45 +39,178 @@ public:
     T2();
 };
 
+class T3: public block
+{
+private:
+    string name = "T3";
+    int Matrix_T3[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 1, 0, 0}, {1, 1, 1, 0}};
+public:
+    T3();
+};
+
+class T4: public block
+{
+private:
+    string name = "T4";
+    int Matrix_T4[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}};
+public:
+    T4();
+};
+
+class L1: public block
+{
+private:
+    string name = "L1";
+    int Matrix_L1[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 0, 0}};
+public:
+    L1();
+};
+
+class L2: public block
+{
+private:
+    string name = "L2";
+    int Matrix_L2[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 0}, {1, 0, 0, 0}};
+public:
+    L2();
+};
+
+class L3: public block
+{
+private:
+    string name = "L3";
+    int Matrix_L3[4][4] = {{0, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}};
+public:
+    L3();
+};
+
+class L4: public block
+{
+private:
+    string name = "L4";
+    int Matrix_L4[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}};
+public:
+    L4();
+};
+
+class J1: public block
+{
+private:
+    string name = "J1";
+    int Matrix_J1[4][4] = {{0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+public:
+    J1();
+};
+
+class J2: public block
+{
+private:
+    string name = "J2";
+    int Matrix_J2[4][4] = {{0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+public:
+    J2();
+};
+
+class J3: public block
+{
+private:
+    string name = "J3";
+    int Matrix_J3[4][4] = {{0, 0, 0, 0}, {1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}};
+public:
+    J3();
+};
+
+class J4: public block
+{
+private:
+    string name = "J4";
+    int Matrix_J4[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {0, 1, 0, 0}, {1, 1, 1, 0}};
+public:
+    J3();
+};
+
+class S1: public block
+{
+private:
+    string name = "S1";
+    int Matrix_S1[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 1, 1, 0}, {1, 1, 0, 0}};
+public:
+    S1();
+};
+
+class S2: public block
+{
+private:
+    string name = "S2";
+    int Matrix_S2[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}};
+public:
+    S2();
+};
+
+class Z1: public block
+{
+private:
+    string name = "Z1";
+    int Matrix_Z1[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 1, 0}};
+public:
+    Z1();
+};
+
+class Z2: public block
+{
+private:
+    string name = "Z2";
+    int Matrix_Z2[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 1, 0}};
+public:
+    Z2();
+};
+
+class I1: public block
+{
+private:
+    string name = "I1";
+    int Matrix_I1[4][4] = {{1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}};
+public:
+    I1();
+};
+
+class I2: public block
+{
+private:
+    string name = "I2";
+    int Matrix_I2[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 1}};
+public:
+    I2();
+};
+
+class O: public block
+{
+private:
+    string name = "O";
+    int Matrix_O [4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 0, 0}, {1, 1, 0, 0}};
+public:
+    O();
+};
 
 int main()
 {
     cout<<"This is a simple tetris game. Enjoy it!"<<endl;
 }
-class form: public block
+class form
 {
+friend class T1, T2, T3, T4, L1, L2, L3, L4, J1, J2, J3, J4, S1, S2, Z1, Z2, O;
 private:
-    
-    
-    int T3[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 1, 0, 0}, {1, 1, 1, 0}};
-    int T4[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}};
-    int L1[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 0, 0}};
-    int L2[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 0}, {1, 0, 0, 0}};
-    int L3[4][4] = {{0, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}};
-    int L4[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}};
-    int J1[4][4] = {{0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
-    int J2[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 1, 0}};
-    int J3[4][4] = {{0, 0, 0, 0}, {1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}};
-    int J4[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {0, 1, 0, 0}, {1, 1, 1, 0}};
-    int S1[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 1, 1, 0}, {1, 1, 0, 0}};
-    int S2[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}};
-    int Z1[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 1, 0}};
-    int Z2[4][4] = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 1, 0}};
-    int I1[4][4] = {{1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}};
-    int I2[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 1}};
-    int O [4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 0, 0}, {1, 1, 0, 0}};
-
-    string form[19] = {"T1", "T2", "T3", "T4", "L1", "L2", "L3", "L4", "J1", "J2", "J3", "J4", "S1", "S2", "S3", "Z1", "Z2", "O"};
+    class form[17] = {"T1", "T2", "T3", "T4", "L1", "L2", "L3", "L4", "J1", "J2", "J3", "J4", "S1", "S2", "Z1", "Z2", "O"};
 public:
 int* get_form()
 {
     string form_name;
     cin>>form_name;
-    for(int i = 0; i< 11; i++)
+    for(int i = 0; i< 17; i++)
     {
         if(form_name == form[i])
         {
-
+            cout<<form[i].name;
         }
     }    
 
